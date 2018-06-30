0822e413480dd20bfcd40783d6683f7ab6833413
Aline <ini.ebone@gmail.com>
2018-06-30 02:44:49 -0300

    Simple list view

---
+++ SimpleListView/app/src/main/java/com/mealine/simplelistview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.mealine.simplelistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        String[] itens = {"Porto Alegre", "São Paulo", "Kokomo", "Leeds", "Aruba", "Bahamas", "Jamaica"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+                );
+
+        listView.setAdapter(adapter);
+
+
+    }
+}
